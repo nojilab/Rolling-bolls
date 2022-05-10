765442b18a955300be6f09f6ba78e1ffc5120ada
Tatsuma-Aoki <tatsuma_aoki@vogue.is.uec.ac.jp>
2022-05-10 20:47:27 +0900

    add PlayerName
     add PlayerName that follow balls.

---
+++ Assets/Scripts/UIFollowTarget.cs
@@ -0,0 +1,20 @@
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIFollowTarget : MonoBehaviour 
+{
+	RectTransform rectTransform = null;
+	[SerializeField] Transform target = null;
+
+	void Awake()
+	{
+		rectTransform = GetComponent<RectTransform> ();
+	}
+
+	void Update()
+	{
+		rectTransform.position = RectTransformUtility.WorldToScreenPoint (Camera.main, target.position);
+	}
+}
